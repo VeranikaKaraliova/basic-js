4db8d923f793f88c92e10c84f2e3d405e9335157
VeranikaKaraliova <nika-11@mail.ru>
2021-03-13 11:24:29 +0300

    feat: add createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(members === null || members === undefined || members.constructor != Array){
+    return false;
+  }
+  else {
+    let nameDreamTeam = [];
+    members.forEach(function(item){
+      if(typeof item === 'string'){
+        nameDreamTeam.push(item.trim().toUpperCase().substr(0,1));
+      };
+    });
+    return nameDreamTeam.sort().join('');
+  };
 };
