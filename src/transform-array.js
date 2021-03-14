46532b94ee14b4e17baaa1c826f733000f81579e
VeranikaKaraliova <nika-11@mail.ru>
2021-03-14 21:37:19 +0300

    feat: add function transform with two error

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let newArr = [];
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] === '--discard-next' && i <(arr.length-1)){
+            i =i+3;
+        }
+        if(arr[i] === '--discard-prev' && i !== 0){
+            newArr = newArr.slice(0, newArr.length-1)
+        }
+        if(arr[i] === '--double-next' && i < (arr.length-1)){
+            newArr.push(arr[i+1])
+            i++
+        }
+        if(arr[i] === '--double-prev' && i !== 0){
+            newArr.push(arr[i-1])
+        }
+        if(typeof arr[i] === 'number') {
+            newArr.push(arr[i])
+        } 
+    }
+    return newArr
 };
