5f84c27820e055ae81627d9afe15bbf1cbd482ce
VeranikaKaraliova <nika-11@mail.ru>
2021-03-28 21:26:15 +0300

    feat: add partial solution function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
+module.exports = function repeater(str, options ) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  let res = []
+
+  for(let i=0; i< options.repeatTimes;i++){
+    res.push(str)
+  }
+  if("separator" in options ){
+      
+      return res.join(options.separator)
+  }else return res.join('+')
 };
   
